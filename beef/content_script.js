9ce8142a82fa683e9b4ef854010ad0637421d596
1891021h <1891021h@stu.kobe-u.ac.jp>
2021-01-24 13:16:36 +0900

    wrote with fetch then

--- beef/content_script.js
+++ beef/content_script.js
@@ -1,12 +1,19 @@
-$(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://beef.center.kobe-u.ac.jp/2020/my/',
-        dataType: 'html',
-        success: function(data) {
-          let task = $(data).find('.list-group-item')
-            console.log(task);
-          $('#frontpage-course-list').append(task);
-        }
-    });
-});
+// $(function() {
+//     $.ajax({
+//         type: 'GET',
+//         url: 'https://beef.center.kobe-u.ac.jp/2020/my/',
+//         dataType: 'html',
+//         success: function(data) {
+//           let task = $(data).find('.list-group-item')
+//             console.log(task);
+//           $('#frontpage-course-list').append(task);
+//         }
+//     });
+// });
+
+fetch('https://beef.center.kobe-u.ac.jp/2020/my/')
+  .then(function(response){ return response.text(); })
+  .then(text => new DOMParser().parseFromString(text, "text/html"))
+  .then(function(text){
+    console.log(text);
+  });
