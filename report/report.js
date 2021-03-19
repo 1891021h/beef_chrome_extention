4961888165955527bdd9f688fe08b8c5f554021a
1891021h <1891021h@stu.kobe-u.ac.jp>
2021-03-19 15:06:22 +0900

    prereporat

---
+++ report/report.js
@@ -0,0 +1,58 @@
+/****** 先週までの特訓を最大2か月分検索 ******/
+// 日付の取得
+const today = new Date();
+let lastMonth = ( '00' + today.getMonth() ).slice( -2 );
+let month = ( '00' + today.getMonth() + 1 ).slice( -2 );
+let day = ( '00' + today.getDate() ).slice( -2 );
+let enc = encodeURI('別所');
+
+// 検索する情報の取得
+let col4 = document.querySelector('#formset-info .col-sm-4').querySelectorAll('.form-group');
+let col3 = document.querySelector('#formset-info .col-sm-3').querySelectorAll('.form-group');
+let col5 = document.querySelector('#formset-info .col-sm-5 .form-group:nth-child(2)').querySelectorAll('.col-sm-6:nth-child(2)');
+
+let dataList = [];
+for(let i = 1; i < col4.length; i++){
+    let value = col4[i].querySelector('.form-control-static').innerText;
+    dataList.push(value);
+}
+for(let i = 0; i < col3.length; i++){
+    let value = col3[i].querySelector('.form-control-static').innerText;
+    dataList.push(value);
+}
+col5.forEach(function(node){
+    time = node.querySelector('.form-control-static').innerText;
+    dataList.push(time);
+});
+
+console.log(dataList);
+//(10) ["個別管理特訓S", "通常特訓", "別所 祐亮", "塾生", "未完了", "大学受験: 英語", "川内 夕佳", "2021/03/16(火) 19:30", "2021/03/16(火) 20:30", "2021/03/16(火) 21:30"]0: "個別管理特訓S"1: "通常特訓"2: "別所 祐亮"3: "塾生"4: "未完了"5: "大学受験: 英語"6: "川内 夕佳"7: "2021/03/16(火) 19:30"8: "2021/03/16(火) 20:30"9: "2021/03/16(火) 21:30"length: 10__proto__: Array(0)
+
+
+//URL生成
+const url = "https://www.takedajuku-system.com/t-student-special-training-schedules/search-list?TStudents%5Blast_name%5D={STLASTNAME}&TStudents%5Bfirst_name%5D={STFIRSTNAME}&TStudentSpecialTrainingSchedules%5Btest_start_date_from%5D=2021%2F{FROMMONTH}%2F{FROMDAY}+00%3A00&TStudentSpecialTrainingSchedules%5Btest_start_date_to%5D=2021%2F{TOMONTH}%2F{TODAY}+{TOTIME}&TLecturers%5Blast_name%5D={LTRLASTNAME}&TLecturers%5Bfirst_name%5D={LTRFIRSTNAME}&search=%E6%A4%9C%E7%B4%A2%E3%81%99%E3%82%8B#tab3";
+
+let toMonth = dataList[7].substr(5,2);
+let toDay = dataList[7].substr(8,2);
+let fromMonth = Number(toMonth) - 1;
+fromMonth = ( '00' + fromMonth).slice( -2 );
+let fromDay = toDay;
+let toTime = dataList[7].substr(14,5);
+toTime = toTime.replace(':','%3A');
+
+let stName = dataList[2].split(' ');
+let ltrName = dataList[6].split(' ');
+
+replaced = url.replace('{FROMMONTH}',fromMonth).replace('{FROMDAY}',fromDay).replace('{TOMONTH}',toMonth).replace('{TODAY}',toDay).replace('{TOTIME}',toTime).replace('{STLASTNAME}',stName[0]).replace('{STFIRSTNAME}',stName[1]).replace('{LTRLASTNAME}',ltrName[0]).replace('{LTRFIRSTNAME}',ltrName[1]);
+console.log(replaced);
+
+//検索結果にアクセス
+fetch(replaced)
+.then(function(response){ return response.text(); })
+.then(text => new DOMParser().parseFromString(text, "text/html"))
+.then(function(text){
+    let preSt = text.querySelector('.table.table-striped.table-hover tbody tr:nth-last-child(2) a').getAttribute('href');
+    let preUrl = 'https://www.takedajuku-system.com/' + preSt;
+    let space = document.querySelector('.content form');
+    space.insertAdjacentHTML('beforebegin','<div class="button"><a href="' + preUrl +'">まずは先週のレポートに宿題達成率・テスト結果を入力してください。</a></div>');
+});
--- report/search.js
+++ report/search.js
@@ -17,27 +17,33 @@ fetch('https://www.takedajuku-system.com/t-students/search-list/02')
     stList.push([stNumber,stName,stFamilyName,stFirstName]);
   })
   setStList(stList);
-  const param = location.search;
-  //console.log(getParam('TStudents%5Blast_name%5D'));
 });
 
+removeClass("btn-primary");
+removeClass("btn-info");
+preWeek();
+makeScheduleTabDefault();
+
+
+/**************************************************
+ * functions
+ **************************************************/
+
+/***** 担当生徒名をプルダウンにセット *****/
 function setStList(stList){
   let target = document.querySelector(".search-in").children[1].firstElementChild.firstElementChild;
   let targetInput = target.querySelector(".text");
   target.removeChild(targetInput);
-  // console.log(target);
-  // console.log(targetInput);
-  //let targetChild = document.querySelector("#tstudents-last-name");
   let element = document.createElement("select");
   element.classList.add("form-control");
   element.setAttribute('name','TStudents[last_name]');
   element.setAttribute('id',"tstudents-last-name");
 
   let optionNow = document.createElement('option');
-  stNow = getParam('TStudents%5Blast_name%5D');
+  let stNow = getParam('TStudents%5Blast_name%5D');
   optionNow.setAttribute('value', stNow);
   optionNow.innerHTML = stNow;
-  element.appendChild(optionNow);
+  element.appendChild(optionNow);//検索中
   
   for(let i = 0; i < stList.length; i++){
     let option = document.createElement('option');
@@ -46,17 +52,23 @@ function setStList(stList){
     console.log(option);
     element.appendChild(option);
   };
-  console.log(element);
+
+  let optionNone = document.createElement('option');
+  optionNone.setAttribute('value', '');
+  optionNone.innerHTML = '指示なし';
+  element.appendChild(optionNone);//検索中
+  
   target.appendChild(element);
-  // element.innerHTML = upcomingEvent;
 }
 
+/* 先週ボタンの表示　*/
 function preWeek(){
   let target = document.querySelector(".btn-warning").parentNode;
   let targetChild = document.querySelector(".btn-warning");
   target.insertAdjacentHTML('afterbegin','<button type=\"button\" class=\"btn btn-primary\" onclick=\"setDay(\'-7\');\">先週</button>');
 }
 
+/* クラスを削除する汎用の関数 */
 function removeClass(text){
   let query = "." + text;
   let target = document.querySelectorAll(query);
@@ -65,21 +77,20 @@ function removeClass(text){
   });
 }
 
-removeClass("btn-primary");
-removeClass("btn-info");
+/* 特訓スケジュールのタブを最初から開く */
+function makeScheduleTabDefault(){
+  let content3 = document.querySelector("#tab3");
+  content3.classList.add("active");
+  let content1 = document.querySelector("#tab1");
+  content1.classList.remove("active");
 
-preWeek();
+  let navTab3 = document.querySelector(".nav-tabs li:nth-child(2)");
+  navTab3.classList.add("active");
+  let navTab1 = document.querySelector(".nav-tabs li:first-child");
+  navTab1.classList.remove("active");
+}
 
-let content3 = document.querySelector("#tab3");
-content3.classList.add("active");
-let content1 = document.querySelector("#tab1");
-content1.classList.remove("active");
-
-let navTab3 = document.querySelector(".nav-tabs li:nth-child(2)");
-navTab3.classList.add("active");
-let navTab1 = document.querySelector(".nav-tabs li:first-child");
-navTab1.classList.remove("active");
-
+/* URLからクエリを取得する */
 function getParam(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -89,17 +100,3 @@ function getParam(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-
-// let target = document.querySelector("#sidr-id-collapseOne ul li a:first-child")
-// target.href = "https://www.takedajuku-system.com/t-student-special-training-schedules/search-list#tab3";
-// console.log(target);
-// console.log(target.href);
-
-
-// https://www.takedajuku-system.com/t-student-special-training-schedules/search-list?TStudents%5Blast_name%5D=&TStudents%5Bfirst_name%5D=&TStudentSpecialTrainingSchedules%5Bpresence_or_absence_cd%5D=&TStudentSpecialTrainingSchedules%5Btest_start_date_from%5D=2021%2F03%2F15+00%3A00&TStudentSpecialTrainingSchedules%5Btest_start_date_to%5D=2021%2F03%2F15+23%3A59&search=%E6%A4%9C%E7%B4%A2%E3%81%99%E3%82%8B#tab3
-
-const today = new Date();
-let lastMonth = ( '00' + today.getMonth() ).slice( -2 );
-let month = ( '00' + today.getMonth() + 1 ).slice( -2 );
-let day = ( '00' + today.getDate() ).slice( -2 );
-let enc = encodeURI('別所');
