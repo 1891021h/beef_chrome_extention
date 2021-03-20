3651d3cd648f7a161b8ad6a2cc94c9bf2ccf2fe1
1891021h <1891021h@stu.kobe-u.ac.jp>
2021-03-20 12:34:23 +0900

    view

--- report/view.js
+++ report/view.js
@@ -1,31 +1,80 @@
+//スマートアラート
+let body = document.querySelector('body');
+let script = document.createElement('script');
+script.setAttribute('src','https://unpkg.com/sweetalert/dist/sweetalert.min.js');
+body.appendChild(script);
+
 let button = document.createElement('div');
 button.setAttribute('style', 'text-align:right;');
 button.innerHTML = '<button id="create">情報送信</button>';
 let target = document.querySelector('.content .clearfix');
 target.insertBefore(button,target.querySelector('h3'));
 
+let reportButtons = document.querySelector('.content').children[1].querySelectorAll('form');
+reportButtons.forEach(function(e){
+    e.classList.add('hidden-btn');
+});
+
 document.getElementById("create").addEventListener("click", function() {
-    pdfMake.fonts = {
-        yourFontName: {
-          normal: 'https://example.com/fonts/fontFile.ttf',
-          bold: 'https://example.com/fonts/fontFile2.ttf',
-          italics: 'https://example.com/fonts/fontFile3.ttf',
-          bolditalics: 'https://example.com/fonts/fontFile4.ttf'
-        }
+    const values = getHomeworkValues();//値取得
+    let elist = [];
+    //宿題それぞれについて
+    for(let i = 0; i < values.length; i++){
+        let homework = values[i];
+        checkTestErrors(homework,elist);
     }
 
-    // JSON形式で content にPDF出力用のデータを設定
-    var pdfContent = {
-        content: [
-            { text: "This is pdfmake test."},
-            { text: "これは等幅ゴシック体(ipagp.tff)のテストです。", font: "IPAmincho"},
-            { text: "これは等幅明朝体(ipamp.tff)のテストです。", font: "IPAgothic"}
-    ],
-    defaultStyle: {
-        font: 'yourFontName'
-      }
+    console.log(elist);
+    if(!elist == ''){
+        window.alert(elist.join('\n'));
     }
+});
 
-    // pdfmakeで content を設定した変数を引数としてPDF生成
-    pdfMake.createPdf(pdfContent).open();
-  })    
+function checkTestErrors(homework,elist){
+    let request = homework[0][3];
+    if(request == ''){
+        elist.push(homework[0][1] + 'のテスト作成依頼が空欄です。');
+    } else{
+        console.log('OK');              
+    }
+}
+
+function getHomeworkValues(){
+    let values = [];
+    let items = document.querySelectorAll('.homework-item');
+    items.forEach(function(e){
+        //左側
+        left = e.querySelector('.col-sm-4').querySelectorAll('.form-group');
+        let leftValues = [];
+        left.forEach(function(d){
+            data = d.children[1].innerText;
+            leftValues.push(data);            
+        });
+
+        //中央
+        centre = e.querySelectorAll('.homework-daily-item');
+        let centreValues = [];
+        centre.forEach(function(c){
+            let tmp = c.querySelectorAll('.col-sm-6');
+            let tmps = [];
+            tmp.forEach(function(b){
+                tmps.push(b.innerText);
+            });
+            centreValues.push(tmps);
+        });
+    values.push([leftValues,centreValues]);
+    });
+    console.log(values);
+    /*
+        [
+            [
+                科目，テキスト，範囲，テスト作成依頼
+            ],
+            [
+                [日付，課題]，[日付，課題]，...
+            ]
+        ]
+
+    */
+   return values;
+}
